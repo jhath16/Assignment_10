6264ba0e4c436a9a8406fe600c34c082e61c0572
jhath16 <hathaway.justin@gmail.com>
2014-09-12 20:39:01 -0400

    finished .search and designed repo-cell

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var gitAPI="https://api.github.com/users/jhath16";
 var reposAPI = "https://api.github.com/users/jhath16/repos";
+var orgsAPI = "https://api.github.com/users/jhath16/orgs";
+var starredAPI ="https://api.github.com/users/jhath16/starred";
 
 
 
