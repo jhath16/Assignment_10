778618287de9859c3a5cf5ca31ed4bf8606710ae
jhath16 <hathaway.justin@gmail.com>
2014-09-13 15:37:56 -0400

    template layout structured for .left

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 var gitAPI="https://api.github.com/users/jhath16";
 var reposAPI = "https://api.github.com/users/jhath16/repos";
 var orgsAPI = "https://api.github.com/users/jhath16/orgs";
+var tiyOrg = "https://api.github.com/orgs/TIY-GVL-FEE-2014-Aug";
 var starredAPI ="https://api.github.com/users/jhath16/starred";
 
 
+function renderTemplate(templateId, container, model) {
+  var templateString = $('#' + templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderedTemplate = templateFunction(model);
+  $(container).append(renderedTemplate);
+}
+
+$.getJSON(gitAPI).done(function(data) {
+  renderTemplate("sidebar",".left", data);   //Use moment.js to convert to correct time
+});
+
+$.getJSON(orgsAPI).done(function(orgs) {
+  var orgUrl = $.getJSON(tiyOrg).done(function(tiy){
+    return tiy.html_url;
+  });
+
+  var avatar = orgs.avatar_url;
+
+});
 
 //$.getJSON(gitAPI, ;
 
